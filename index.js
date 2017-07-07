eec5619bff6ec9d7dbf8f96f040824da0629749b
marigerr <marigerr.sv@gmail.com>
2017-07-07 08:08:26 +0200

    moved buttons

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 
-$( document ).ready(function() {
+$(document).ready(function () {
   getNewQuote();
-  $("#new-quote").on("click", getNewQuote);  
+  $("#new-quote").on("click", getNewQuote);
   // $("#tweet-quote").on("click", tweetQuote);
   console.log(find("#tweet-quote").onclick);
 
-// var elem = document.getElementById("tweet-quote");
-// var events = (jQuery._data || jQuery.data)(elem, 'events');
-// console.log(events);
+  // var elem = document.getElementById("tweet-quote");
+  // var events = (jQuery._data || jQuery.data)(elem, 'events');
+  // console.log(events);
 
   // console.log(document.getElementById("tweet-quote"));
   // jQuery( document.getElementById("tweet-quote") ).data( "events" );
   // console.log(document.getElementById("#tweet-quote").onclick);
-      
+
   //$("#tweet-quote").hide();
 });
 
-var getNewQuote = function(){
-    $.getJSON('https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', function(json) {
-      console.log(json.quoteText.length);
-       $("#text").html(json.quoteText);
-       if (json.quoteAuthor == "") {
-        $("#author").html("Anonymous");  
-       } else {
-        $("#author").html(json.quoteAuthor);
-        json.quoteAuthor.split(' ').join('_');
-        $("#author").attr("href", "https://en.wikipedia.org/wiki/" + json.quoteAuthor.split(' ').join('_'));
-       }
-       
-       $("#tweet-quote").attr("href", 'https://twitter.com/intent/tweet' + '?text=' + json.quoteText + ' - ' + json.quoteAuthor)
-       $("#wikiBtn").attr("href", "https://en.wikipedia.org/wiki/" + json.quoteAuthor.split(' ').join('_'));       
-    });
-    
+var getNewQuote = function () {
+  $.getJSON('https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', function (json) {
+    console.log(json.quoteText.length);
+    $("#text").html(json.quoteText);
+    if (json.quoteAuthor == "") {
+      $("#author").html("Anonymous");
+    } else {
+      $("#author").html(json.quoteAuthor);
+      json.quoteAuthor.split(' ').join('_');
+      $("#author").attr("href", "https://en.wikipedia.org/wiki/" + json.quoteAuthor.split(' ').join('_'));
+    }
+
+    $("#tweet-quote").attr("href", 'https://twitter.com/intent/tweet' + '?text=' + json.quoteText + ' - ' + json.quoteAuthor)
+    $("#wikiBtn").attr("href", "https://en.wikipedia.org/wiki/" + json.quoteAuthor.split(' ').join('_'));
+  });
+
 };
 
 // var tweetQuote = function(){
